c12c8ef08a133abb909268e9a19879a918388f11
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-02-26 23:34:58 +0100

    complete portal test

--- src/__tests__/portals.js
+++ src/__tests__/portals.js
@@ -3,14 +3,18 @@ import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 
 // 0⃣ 🐨 you'll need these
-// import React from 'react'
-// import {render} from 'react-testing-library'
-// import {Modal} from '../modal'
+import React from 'react'
+import {render} from 'react-testing-library'
+import {Modal} from '../modal'
 
 test('modal shows the children', () => {
-  // 1⃣ 🐨 render the modal with anything you want as the children
-  // 2⃣ 🐨 add an assertion that what you want is in the document.
-  // seriously... that's it...
+  const {getByText} = render(
+    <Modal>
+      <div> modal content</div>
+    </Modal>,
+  )
+
+  expect(getByText(/modal content/i)).toBeInTheDocument()
 })
 
 //////// Elaboration & Feedback /////////
