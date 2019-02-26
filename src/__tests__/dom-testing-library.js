8b7a40cf5298f6c8a861ecd2e0fc3039fb29b670
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-02-26 01:48:18 +0100

    complete dom-testing-lib test

--- src/__tests__/dom-testing-library.js
+++ src/__tests__/dom-testing-library.js
@@ -1,6 +1,7 @@
 import 'jest-dom/extend-expect'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {getQueriesForElement} from 'react-testing-library'
 // 🐨 you'll need the getQueriesForElement function from 'dom-testing-library'
 // 📖 read more in the docs: https://github.com/kentcdodds/dom-testing-library/blob/7cb84a9068fd04d17d89edb8988fcc181a40becf/README.md#within-and-getqueriesforelement-apis
 import {FavoriteNumber} from '../favorite-number'
@@ -9,11 +10,17 @@ test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
   // use getQueriesForElement with the `div` to get the "getByLabelText" query
+  const {getByLabelText, getByText} = getQueriesForElement(div)
   // use the "getByLabelText" query to get the input, then you can
+
+  const input = getByLabelText(/Favorite Number/i)
+  const label = getByText(/Favorite Number/i)
+  expect(input).toHaveAttribute('type', 'number')
+  expect(label).toHaveTextContent('Favorite Number')
   // remove the assertion on the label.
   // 📖 read more in the docs: https://github.com/kentcdodds/dom-testing-library/blob/7cb84a9068fd04d17d89edb8988fcc181a40becf/README.md#getbylabeltext
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  //expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+  //expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
 
 //////// Elaboration & Feedback /////////
@@ -25,8 +32,8 @@ test('renders a number input with a label "Favorite Number"', () => {
 /*
 http://ws.kcd.im/?ws=react-testing-library-course&e=dom-testing-library&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
