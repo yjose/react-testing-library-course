4f39ce0f3706cfd8fc08fb8778f1713af51a96f5
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-02-26 01:26:00 +0100

    complete react-dom.js test

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,13 +1,21 @@
 // 🐨 you're going to need to use React to create react elements, so import react
+import React from 'react'
+import ReactDOM from 'react-dom'
 // 🐨 we're going to render the FavoriteNumber component with ReactDOM so you'll need to import react-dom
 // 🐨 Here's your component:
-// import {FavoriteNumber} from '../favorite-number'
+import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
   // 🐨 create a div (💯 document.createElement)
+  const rootEl = document.createElement('div')
   // 🐨 render the FavoriteNumber component to that div with ReactDOM.render
+  ReactDOM.render(<FavoriteNumber />, rootEl)
   // 🐨 assert the input type attribute is a number
+  const inputType = rootEl.querySelector('input').type
+  expect(inputType).toBe('number')
   //:🐨 assert the label's text content is "Favorite Number"
+  const inputContent = rootEl.querySelector('label').textContent
+  expect(inputContent).toBe('Favorite Number')
 })
 
 //////// Elaboration & Feedback /////////
@@ -19,8 +27,8 @@ test('renders a number input with a label "Favorite Number"', () => {
 /*
 http://ws.kcd.im/?ws=react-testing-library-course&e=react-dom&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
