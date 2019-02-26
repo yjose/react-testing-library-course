5afca0fc6574743feb13b96abec81cc9c8ebd870
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-02-27 00:06:25 +0100

    complete http-jest-mock test

--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -3,10 +3,10 @@ import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 
 // 0⃣ 🐨 you're going to need these
-// import React from 'react'
-// import {render, fireEvent, wait} from 'react-testing-library'
-// import {loadGreeting as mockLoadGreeting} from '../api'
-// import {GreetingLoader} from '../greeting-loader-01-mocking'
+import React from 'react'
+import {render, fireEvent, wait} from 'react-testing-library'
+import {loadGreeting as mockLoadGreeting} from '../api'
+import {GreetingLoader} from '../greeting-loader-01-mocking'
 
 // our component makes an HTTP request when you click on the load button.
 // we don't want it to do this for various reasons, so instead we'll mock
@@ -14,20 +14,33 @@ import 'react-testing-library/cleanup-after-each'
 // a fake version of what we want it to return.
 // 4⃣ 🐨 use jest.mock to mock the '../api' module and return a fake `loadGreeting`:
 // 💯 jest.fn(subject => Promise.resolve({data: {greeting: `Hi ${subject}`}})
+jest.mock('../api', () => ({
+  loadGreeting: jest.fn(subject =>
+    Promise.resolve({data: {greeting: `Hi ${subject}`}}),
+  ),
+}))
 
 // 👀 notice this as an async test:
 test('loads greetings on click', async () => {
   // 1⃣ 🐨 render the GreetingLoader component
+  const {getByLabelText, getByText, getByTestId} = render(<GreetingLoader />)
   //
   // 2⃣ 🐨 set the name input's value to whatever you like
-  //
+  const input = getByLabelText(/Name/i)
+  fireEvent.change(input, {target: {value: 'Youssouf'}})
   // 3⃣ 🐨 use fireEvent to click on the load button
-  //
+  fireEvent.click(getByText(/Load Greeting/i))
   // 5⃣ 🐨 make an assertion that your mocked loadGreeting function was called once
+  expect(mockLoadGreeting).toBeCalled()
+  expect(mockLoadGreeting).toBeCalledTimes(1)
+  expect(mockLoadGreeting).toBeCalledWith('Youssouf')
   // and that it was called with the value you set to the name input's value.
   //
   // 6⃣ 🐨 use react-testing-library's `wait` utility to wait until the `greeting`
   // node has the correct text content.
+  await wait(() =>
+    expect(getByTestId('greeting')).toHaveTextContent(`Hi Youssouf`),
+  )
   // 📖 https://github.com/kentcdodds/react-testing-library/blob/b18ff5b96210a887e784b9f53bd886e11b6ed5e0/README.md#wait
 })
 
@@ -40,8 +53,8 @@ test('loads greetings on click', async () => {
 /*
 http://ws.kcd.im/?ws=react-testing-library-course&e=http-jest-mock&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
