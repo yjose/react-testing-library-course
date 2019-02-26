3ecd6bf35bb469731c41c95e8b7412d1443f8351
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-02-26 02:20:56 +0100

    complete prop-update test

--- src/__tests__/prop-updates.js
+++ src/__tests__/prop-updates.js
@@ -7,12 +7,16 @@ import {FavoriteNumber} from '../favorite-number'
 test('entering an invalid value shows an error message', () => {
   // 🐨 you'll need to add queryByTestId, and rerender
   // 📖 learn how queryBy* queries differ from getBy* queries: https://github.com/kentcdodds/react-testing-library/blob/61e382f10d2d8d0be458103b7c267101541ed952/README.md#query-apis
-  const {getByLabelText, getByTestId} = render(<FavoriteNumber />)
+  // const {getByLabelText, getByTestId} = render(<FavoriteNumber />)
+  // const input = getByLabelText(/favorite number/i)
+  // fireEvent.change(input, {target: {value: 10}})
+  // expect(getByTestId('error-message')).toHaveTextContent(
+  //   /the number is invalid/i,
+  // )
+  const {getByLabelText, queryByTestId} = render(<FavoriteNumber max={20} />)
   const input = getByLabelText(/favorite number/i)
-  fireEvent.change(input, {target: {value: 10}})
-  expect(getByTestId('error-message')).toHaveTextContent(
-    /the number is invalid/i,
-  )
+  fireEvent.change(input, {target: {value: 11}})
+  expect(queryByTestId('error-message')).toBeNull()
   // 🐨 call rerender with the FavoriteNumber and a max prop of 10.
   // 🐨 use the queryByTestId to verify that the 'error-message' node is not rendered.
 })
@@ -26,8 +30,8 @@ test('entering an invalid value shows an error message', () => {
 /*
 http://ws.kcd.im/?ws=react-testing-library-course&e=prop-updates&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
